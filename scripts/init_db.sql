0e2e60e9fdec023870fa183c9a1d348d32ee3936
Humslyng mumali <123473498+lx3002@users.noreply.github.com>
2026-04-28 15:08:20 +0300

    Initialize DataWarehouse database and schemas
    Drop existing DataWarehouse database if it exists and create a new one with schemas.

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -1,1 +1,28 @@
 
+
+use master;
+GO
+
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN 
+   ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+   DROP DATABASE DataWarehouse;
+END;
+GO
+
+CREATE DATABASE DataWarehouse;
+GO
+
+use DataWarehouse;
+GO
+
+
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
